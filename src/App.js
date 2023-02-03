247d600994cbd1c8fe306e95eadb69869e435891
unknown <sathishsatish2002@gmail.com>
2023-02-03 11:05:22 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import React from 'react'
 import { Route, Routes } from 'react-router-dom';
 import { CreateContainer, Header, MainContainer } from './components';
 import {AnimatePresence} from 'framer-motion';
