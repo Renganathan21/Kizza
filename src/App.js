dc97779b65d650ce82a4f27ede315d7754d495ce
Renganathan21 <114427016+Renganathan21@users.noreply.github.com>
2023-02-03 11:06:04 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from 'react'
 import { Route, Routes } from 'react-router-dom';
 import { CreateContainer, Header, MainContainer } from './components';
 import {AnimatePresence} from 'framer-motion';
